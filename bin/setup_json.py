f1841f8b9fcbe05335ab9c7094e5b0b7ede09bcf
Kevin <walchko@users.noreply.github.com>
2016-09-13 11:00:49 -0600

    Update setup_json.py

--- bin/setup_json.py
+++ bin/setup_json.py
@@ -7,6 +7,7 @@
 
 from __future__ import print_function, division
 from pyxl320 import DummySerial
+# from pyxl320 import ServoSerial
 from pyxl320 import Packet, xl320
 from pyxl320.Packet import le
 # import simplejson as json
@@ -32,92 +33,19 @@ xl320_func = {
 	"led": [Packet.makeLEDPacket]
 }
 
-
-# class JsonFile(object):
-# 	"""
-# 	Simple class to handle json files.
-# 	"""
-# 	@staticmethod
-# 	def read(fname):
-# 		"""
-# 		Reads a Json file
-# 		in: file name
-# 		out: length of file, dictionary
-# 		"""
-# 		try:
-# 			with open(fname, 'r') as f:
-# 				data = json.load(f)
-# 			return data
-#
-# 		except IOError:
-# 			raise Exception('Could not open {0!s} for reading'.format((fname)))
-#
-# 	@staticmethod
-# 	def write(fname, data):
-# 		"""
-# 		Writes a Json file
-# 		"""
-# 		try:
-# 			with open(fname, 'w') as f:
-# 				json.dump(data, f)
-#
-# 		except IOError:
-# 			raise Exception('Could not open {0!s} for writing'.format((fname)))
-
-# data = {
-# 	1: {
-# 		"XL320_CW_ANGLE_LIMIT": 200,
-# 		"XL320_CCW_ANGLE_LIMIT": 20
-# 	},
-# 	2: {
-# 		"XL320_CW_ANGLE_LIMIT": 200,
-# 		"XL320_CCW_ANGLE_LIMIT": 20
-# 	}
-# }
-
-# data = {
-# 	1: {
-# 		xl320.XL320_CW_ANGLE_LIMIT: 200,
-# 		xl320.XL320_CCW_ANGLE_LIMIT: 20
-# 	},
-# 	2: {
-# 		xl320.XL320_CW_ANGLE_LIMIT: 200,
-# 		xl320.XL320_CCW_ANGLE_LIMIT: 20
-# 	}
-# }
-
 data = JsonFile.read('test.json')
 print(data)
-# exit()
-#
-# data = [
-# 	{
-# 		xl320.XL320_ID: 1,
-# 		xl320.XL320_CW_ANGLE_LIMIT: 200,
-# 		xl320.XL320_CCW_ANGLE_LIMIT: 20
-# 	},
-# 	{
-# 		xl320.XL320_ID: 2,
-# 		xl320.XL320_CW_ANGLE_LIMIT: 200,
-# 		xl320.XL320_CCW_ANGLE_LIMIT: 20
-# 	}
-# ]
 
 def main():
 	filename = 'test.json'
-	port = 'a'
+	port = '/dev/tty.usbserial0'
+	
+	data = JsonFile.read(filename)
+	print(data)
 
+	# ser = ServoSerial(port)
 	ser = DummySerial(port)
 	ser.open()
-
-	# jf = JsonFile()
-	# data = jf.readJson(filename)
-
-
-
-	# print(data)
-	# print('xl320.XL320_ID:', data[1][xl320.XL320_ID])
-	# exit()
 
 	for servo in data:
 		ID = servo['id']
@@ -125,26 +53,15 @@ def main():
 		for key, val in servo.items():
 			print('> setting servo[{}]: {} {}'.format(ID, key, val))
 
-			# reg = key
-			# reg = xl320_reg[key][0]
-			# if xl320_reg[key][1] == 1:
-			# 	param = val
-			# else:
-			# 	param = val
-
-			# pkt = Packet.makeWritePacket(ID, reg, param)
 			if key in xl320_func:
-				func = xl320_func[key][0]
-				pkt = func(ID, val)
-				print('pkt', pkt)
-				ser.write(pkt)
-				ser.read()
+				func = xl320_func[key][0]  # grab correct function
+				pkt = func(ID, val)  # build packet
+				# print('pkt', pkt)
+				ser.sendPkt(pkt)  # send packet
+			
 			else:
-				print('>> Error: {} not found'.format(key))
+				print('>> Error: {} not currently supported'.format(key))
 
-			# the servo id changed, so update it
-			# if key == 'XL320_ID':
-			# 	curr_id = val
 
 if __name__ == '__main__':
 	main()
