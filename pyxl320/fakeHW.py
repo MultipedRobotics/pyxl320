4b884327525c41c47b87c32a04e2a35cf693aa7b
Kevin <walchko@users.noreply.github.com>
2017-03-27 17:26:07 -0600

    don't need

--- pyxl320/fakeHW.py
+++ pyxl320/fakeHW.py
@@ -1,22 +1,22 @@
 # import random
-from __future__ import print_function
+# from __future__ import print_function
 
-print('WARNING: using fake RPi.GPIO')
+# print('WARNING: using fake RPi.GPIO')
 
-class GPIO(object):
-	IN = True
-	OUT = False
-	BCM = True
-	def __init__(self): print('dummy GPIO')
-	@staticmethod
-	def setwarnings(a): pass
-	@staticmethod
-	def setmode(a): pass
-	@staticmethod
-	def setup(a, b): pass
-	@staticmethod
-	def input(a): return 1
-	@staticmethod
-	def cleanup(): pass
-	@staticmethod
-	def output(a, b): pass
+# class GPIO(object):
+# 	IN = True
+# 	OUT = False
+# 	BCM = True
+# 	def __init__(self): print('dummy GPIO')
+# 	@staticmethod
+# 	def setwarnings(a): pass
+# 	@staticmethod
+# 	def setmode(a): pass
+# 	@staticmethod
+# 	def setup(a, b): pass
+# 	@staticmethod
+# 	def input(a): return 1
+# 	@staticmethod
+# 	def cleanup(): pass
+# 	@staticmethod
+# 	def output(a, b): pass
