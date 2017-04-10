a89c3e57a8ecfe6829eea5624a4c3f00d9a0d348
Kevin <walchko@users.noreply.github.com>
2017-04-09 23:04:58 -0600

    build_utils

--- setup.py
+++ setup.py
@@ -9,56 +9,25 @@
 from __future__ import print_function
 from setuptools import setup
 from pyxl320 import __version__ as VERSION
-import os
-from setuptools.command.test import test as TestCommand
-from setuptools.dist import Distribution
+from build_utils import BuildCommand
+from build_utils import PublishCommand
+from build_utils import BinaryDistribution
 
 
-class BinaryDistribution(Distribution):
-	def is_pure(self):
-		return False
-
-
-class BuildCommand(TestCommand):
-	"""Build binaries/packages"""
-	def run_tests(self):
-		print('Delete dist directory and clean up binary files')
-		os.system('rm -fr dist')
-		os.system('rm pyxl320/*.pyc')
-		os.system('rm pyxl320/__pycache__/*.pyc')
-		print('Run Nose tests')
-		print('Python2 tests')
-		ret = os.system("python2 -m nose -v -w tests test.py")
-		if ret > 0:
-			print('<<< Python2 nose tests failed >>>')
-			return
-		print('Python3 tests')
-		ret = os.system("python3 -m nose -v -w tests test.py")
-		if ret > 0:
-			print('<<< Python3 nose tests failed >>>')
-			return
-
-		print('Building packages ...')
-		os.system("python setup.py sdist")
-		os.system("python2 setup.py bdist_wheel")
-		os.system("python3 setup.py bdist_wheel")
-
-
-class PublishCommand(TestCommand):
-	"""Publish to Pypi"""
-	def run_tests(self):
-		print('Publishing to PyPi ...')
-		os.system("twine upload dist/pyxl320-{}*".format(VERSION))
+PACKAGE_NAME = 'pyxl320'
+BuildCommand.pkg = PACKAGE_NAME
+PublishCommand.pkg = PACKAGE_NAME
+PublishCommand.version = VERSION
 
 
 setup(
 	author='Kevin Walchko',
 	author_email='kevin.walchko@outlook.com',
-	name='pyxl320',
+	name=PACKAGE_NAME,
 	version=VERSION,
 	description='A library to control dynamixel XL-320 servos with python',
 	long_description=open('README.rst').read(),
-	url='http://github.com/walchko/pyxl320',
+	url='http://github.com/walchko/{}'.format(PACKAGE_NAME),
 	classifiers=[
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
@@ -72,7 +41,7 @@ setup(
 	],
 	license='MIT',
 	keywords='dynamixel xl320 xl-320 servo actuator library robotics spider',
-	packages=['pyxl320'],
+	packages=[PACKAGE_NAME],
 	install_requires=['pyserial', 'simplejson'],
 	cmdclass={
 		'publish': PublishCommand,
